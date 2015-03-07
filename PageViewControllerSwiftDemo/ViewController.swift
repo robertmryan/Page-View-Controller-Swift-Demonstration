ec1214f53fb9560b3f8a26bed86017cd0afb32bd
Rob Ryan <robert.ryan@mindspring.com>
2015-03-07 12:14:39 -0500

    Initial Commit

---
+++ PageViewControllerSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageViewControllerSwiftDemo
+//
+//  Created by Robert Ryan on 3/7/15.
+//  Copyright (c) 2015 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
